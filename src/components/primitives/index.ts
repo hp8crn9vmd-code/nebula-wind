d2d51468e78f304be465ce2a8521850a9760444e
hp8crn9vmd-code <hp8crn9vmd@privaterelay.appleid.com>
2025-12-24 17:04:17 +0000

    ui: widgets (alert/badge/card) + backup cleanup

---
+++ src/components/primitives/index.ts
@@ -0,0 +1,6 @@
+export { default as Box } from './Box.astro';
+export { default as Stack } from './Stack.astro';
+export { default as Text } from './Text.astro';
+export { default as Heading } from './Heading.astro';
+export { default as Link } from './Link.astro';
+export { default as Button } from './Button.astro';
---
+++ src/components/widgets/index.ts
@@ -0,0 +1,3 @@
+export { default as Alert } from "./Alert.astro";
+export { default as Badge } from "./Badge.astro";
+export { default as Card } from "./Card.astro";
